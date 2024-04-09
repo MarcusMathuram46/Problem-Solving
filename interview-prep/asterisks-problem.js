b96ce45a84ccfc41d7f3cc66d2bed199624ea48f
Marcus Mathuram <marcus.mathuram7@gmail.com>
2024-04-09 15:02:35 +0530

    done

---
+++ interview-prep/asterisks-problem.js
@@ -0,0 +1,14 @@
+// *****
+// ****
+// ***
+// **
+// *
+// code Here
+
+for (let i = 5; i >= 1; i--){
+  let asterisks = '';
+  for (let j = 1; j <= i; j++) {
+    asterisks += '*';
+  }
+  console.log(asterisks);
+}
